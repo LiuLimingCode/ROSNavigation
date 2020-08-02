108ec01426234468e5c2344df435bee504a3cc0b
TURBOyan <turbo1928@icloud.com>
2020-08-02 22:16:28 +0800

    车的结构完成

--- src/racecar_hardware/art_driver/src/art_racecar.cpp
+++ src/racecar_hardware/art_driver/src/art_racecar.cpp
@@ -11,17 +11,27 @@
 
 std::string ackermann_cmd_topic;
 
+uint16_t angle_pwm_max_us=2303;
+uint16_t angle_pwm_min_us=1090;
+uint16_t speed_pwm_max_us=1600;
+uint16_t speed_pwm_min_us=1350;
+
+double speed_max_m_s=3.0;
+
+double PI=3.14159265354;
+
 void AckermannCallback(const ackermann_msgs::AckermannDriveStamped& Ackermann)
 {
-    double angle;
+    uint16_t angle;
+    uint16_t speed;
     
-    
-    
-    angle = 2500.0 - Ackermann.drive.steering_angle * 2000.0 / 180.0;
-    ROS_INFO("Ackermann.drive.speed= %f", Ackermann.drive.speed);
-    ROS_INFO("Ackermann.drive.steering_angle= %f", Ackermann.drive.steering_angle);
-    ROS_INFO("angle= %d",uint16_t(angle));
-    send_cmd(uint16_t(Ackermann.drive.speed),uint16_t(angle));
+    angle = (Ackermann.drive.steering_angle+PI/4.0)*(2/PI)*(angle_pwm_max_us-angle_pwm_min_us)+angle_pwm_min_us;
+    //根据公式->{（angle-pwm_min）/（pwm_max+pwm_min）}*(PI/2.0)-(PI/4.0)=Ackermann.drive.steering_angle得出，将转向的角度值转换为高电平时间（us）
+    speed = (Ackermann.drive.speed/speed_max_m_s)*(speed_pwm_max_us-speed_pwm_min_us)+speed_pwm_min_us;
+    //根据公式->{（speed-pwm_min）/（pwm_max+pwm_min）}*speed_max_m_s得出，将设定的速度（m/s）转换为电机pwm的高电平时间
+    ROS_INFO("speed= %d", speed);
+    ROS_INFO("angle= %d", angle);
+    send_cmd(uint16_t(speed),uint16_t(angle));
 }
 
 int main(int argc, char** argv)
